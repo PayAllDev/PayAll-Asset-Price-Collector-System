ff890ac1f9f773ed0dff3565a2abf24ab723be11
Armando Peralta (XxTavo16xX) <armandoperalta@payall.com.mx>
2021-10-29 07:06:31 -0500

    The last date modification credit has been changed

--- SharedFunctions.js
+++ SharedFunctions.js
@@ -4,7 +4,7 @@
 -- @Author: Armando Peralta
 -- @Version: 2.2.0
 -- @Date: 01/10/2021
--- @Last Modification Date: 20/10/2021
+-- @Last Modification Date: 29/10/2021
 -- @Description: This file have multiple functions that are used by index.js
 -- @Programming Language: Javascript
 
