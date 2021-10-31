cb521828896e7296818c16d0e691b3c1fa807fd5
Armando Octavio Peralta Ramirez <68368658+XxTavo16xX@users.noreply.github.com>
2021-10-31 04:41:53 -0600

    Update SharedFunctions.js

--- SharedFunctions.js
+++ SharedFunctions.js
@@ -2,7 +2,7 @@
 
 -- PayAll System - SharedFunctions
 -- @Author: Armando Peralta
--- @Version: 2.2.0
+-- @Version: 2.2.1
 -- @Date: 24/10/2021
 -- @Last Modification Date: 30/10/2021
 -- @Description: This file have multiple functions that are used by index.js
